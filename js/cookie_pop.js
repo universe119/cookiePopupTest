90af5325cc479a41aeb359e9ccc3c4e2c81873ad
tubi55 <tubi55@nate.com>
2024-09-23 16:36:20 +0900

    쿠키값 갱신은 화면을 새로고침 시 반영

--- js/cookie_pop.js
+++ js/cookie_pop.js
@@ -1,18 +1,3 @@
-/*
-  Cookie : 사용자 브라우저에 물리적인 형태로 특정 데이터를 저장하는 경량의 텍스트 자료 4kb
-  사용자 브라우저에 특정 사용자 정보값을 저장해서 웹사이트에서 쿠키에 저장된 값을 기억해 활용하기 위함
-  사용예1 : 오늘하루 광고창 보지않기
-  사용예2 : 쇼핑몰에 있는 전달 담아놓은 장바구니 정보 출력
-  사용예3 : 특정 사이트 접속하고 로그인시 '홍길동'님 반갑습니다.
-
-  응용예 : 사용자가 우리가 개발한 웹 페이지 방문시 각 사용자가 좋아하는 색상을 선택하게 한 뒤,
-  해당 정보값을 사용자 컴퓨터에 쿠키로 저장
-  앞으로 해당 사용자가 웹페이지 접속할 때마다 사용자 브라우저 쿠키에 저장된 값으로 웹페이지 스타일 테마 변경
-
-  쿠키 : name=value 형식으로 저장, 쿠키 생성시 쿠키의 만료일을 설정가능
-  쿠키확인 : document.cookie
-*/
-
 const [btnView, btnSet] = document.querySelectorAll("button");
 
 btnView.addEventListener("click", () => {
@@ -20,23 +5,16 @@ btnView.addEventListener("click", () => {
 });
 
 btnSet.addEventListener("click", () => {
-	//today=done이라는 이름으로 쿠키 생성함과 동시에 만료시간을 1분을 지정하여
-	//쿠키가 생성된 시점부터 1분까지만 유지되고 1분뒤에는 자동으로 쿠키 제거됨
-	setCookie("today", "done", 1);
+	setCookie("today", "done", 10);
 });
 
 //쿠키 생성 함수
-function setCookie(name, value, min) {
+function setCookie(name, value, sec) {
 	let now = new Date();
-	//현재 분값을 가져와서 인수로 전달된 분 시간정보를 더함
-	let duedate = now.getMinutes() + min;
-	//바뀐 시간 정보값으로 시간객체정보를 변경
-	now.setMinutes(duedate);
-	//변경된 시간 정보값을 표준시로 변경해서 쿠키만료시간으로 설정
+	//아래와 같이 getTime으로 현재 시간값을 가져와서 초단위로 값을 변경하면, 잘못된 날짜 정보로 만료일 설정을 예방
+	let duedate = now.getTime() + sec * 1000;
+	//쿠키값은 화면 새로고침되어야지만 갱신된값이 반영
+	now.setTime(duedate);
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
 	alert("쿠키 생성");
 }
-
-//위와 같이 쿠키의 만료시간을 second, minutes, date로 지정하면
-//쿠키가 생성되는 시점에 만료일이 설정되다보니 64초, 65분, 35일 같은 존재하지 않는 값으로 만료일이 잘못 세팅되는 이슈
-//해결 방법은 getTime()을 이용해서 : 표준시가 설정된 이후부터를 초단위로 연산해서 반환된 값으로 설정
