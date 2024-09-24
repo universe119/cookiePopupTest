db870df61f51e17be8aba312cf30e0acebc7a8e1
universe119 <ruddh119@gmail.com>
2024-09-24 09:55:59 +0900

    쿠키 닫기버튼 모달 안보이게 추가

--- js/cookie_pop.js
+++ js/cookie_pop.js
@@ -1,4 +1,16 @@
+/*
+문자열.indexOf("찾을 문자열") : 전체 문자열에서 찾을 문자열이 위치해있는 순서값 반환, 만약 전체 문자열에서 찾을 문자열이 없으면 -1을 반환 
+
+1. 모달의 닫기 버튼 클릭시 모달 안보이게 하기
+2. 체크박스 선택한 뒤 닫기버튼 클릭시 모달 안보이게 함과 동시에 today=done 쿠키를 하루만료기한 생성
+3. 스크립트가 처음 로딩될때 조건문으로 today=done이라는 쿠키가 있으면 팝업안보이게 처리. 없으면 보이게 처리
+*/
+
 const [btnView, btnSet, btnDel] = document.querySelectorAll("button");
+
+const modal = document.querySelector("aside");
+const btnClose = modal.querySelector("button");
+const ck = modal.querySelector("#ck");
 
 btnView.addEventListener("click", () => {
 	console.log(document.cookie);
@@ -12,14 +24,17 @@ btnDel.addEventListener("click", () => {
 	setCookie("today", "done", 0);
 });
 
+// 닫기 버튼 클릭시 모달 안보임 처리
+btnClose.addEventListener("click", () => {
+	modal.style.display = "none";
+});
+//
+
 //쿠키 생성 함수
-function setCookie(name, value, min) {
+function setCookie(name, value, day) {
 	let now = new Date();
-	//아래와 같이 getTime으로 현재 시간값을 가져와서 초단위로 값을 변경하면, 잘못된 날짜 정보로 만료일 설정을 예방
-	let duedate = now.getTime() + min * 1000 * 60;
-	//쿠키값은 화면 새로고침되어야지만 갱신된값이 반영
+	let duedate = now.getTime() + day * 1000 * 60 * 60 * 24;
+
 	now.setTime(duedate);
 	document.cookie = `${name}=${value}; path=/; expires=${now.toUTCString()}`;
-
-	min === 0 ? alert("쿠키 삭제") : alert("쿠키 생성");
 }
