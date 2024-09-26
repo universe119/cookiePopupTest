1686a0eb6cd75d7147baf1927084d0e97c4e0e6d
universe119 <ruddh119@gmail.com>
2024-09-26 23:41:12 +0900

    쿠키 삭제 추가

--- js/cookie_pop.js
+++ js/cookie_pop.js
@@ -64,7 +64,7 @@ function setCookie(name, value, day) {
   쿠키확인 : document.cookie
 */
 
-/* 쿠키 생성 함수 (코드 오류 수정)
+/* 쿠키 생성 함수 최종 수정.
 const [btnView, btnSet] = document.querySelectorAll("button");
 btnView.addEventListener("click", () => {
 	console.log(document.cookie);
@@ -76,11 +76,16 @@ btnSet.addEventListener("click", () => {
 	setCookie("today", "done", 1);
 });
 
+btnSet.addEventListener("click", () => {
+	setCookie("today", "done", 0);
+	alert("쿠키 삭제");
+});
+
 // 쿠키 생성 함수
 function setCookie(name, value, min) {
 	let now = new Date();
 	//아래와 같이 getTime으로 현재 시간값을 가져와서 초단위로 값을 변경하면, 잘못된 날짜 정보로
-	let duedate = now.getTime() + sec * 1000;
+	let duedate = now.getTime() + min * 1000 * 60;
 	//쿠키값은 화면 새로고침되어야지만 갱신된값이 반영
 	now.setTime(duedate);
 
